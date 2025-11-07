e55c112fd922bcffa18447c1b2c0465b6cc73ec3
ahmednurmok <ahmednurmok@gmail.com>
2025-11-07 11:24:15 +0000

    Add files via upload

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,9 @@
+
+//Task: Style the second list item to become purple
+
+document.querySelectorAll("li")[1].style.color="purple";
+
+//Task: Style the anchor tag containing the google link to become red
+document.querySelector("li a").style.color = "red";
+
+
